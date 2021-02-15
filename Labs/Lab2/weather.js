f66e1762f4ec3a8ae3b3de354382ceb014de984a
Manya <treham@rpi.edu>
2021-02-15 10:21:07 -0500

    Rounded Temperature

--- Labs/Lab2/weather.js
+++ Labs/Lab2/weather.js
@@ -13,9 +13,9 @@ $.getJSON(
   "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=68a01e80027a3c2fe93cc100dc4e0a80&units=imperial",
   function(data) {
       document.getElementById('city').innerHTML ="<b>"+ data.name +" "+ "Weather"+"</b>"
-      document.getElementById('temp').innerHTML = data.main.temp+ "&deg;F"
-      document.getElementById('feelslike').innerHTML ="Feels Like"+" "+ data.main.feels_like+ "&deg;F"
-      document.getElementById('icon').innerHTML ="<img src='http://openweathermap.org/img/w/" + data.weather[0].icon +".png'>"
+      document.getElementById('temp').innerHTML = Math.round(data.main.temp)+ "&deg;F"
+      document.getElementById('feelslike').innerHTML ="Feels Like"+" "+ Math.round(data.main.feels_like)+ "&deg;F"
+      document.getElementById('icon').innerHTML ="<img src='http://openweathermap.org/img/wn/" + data.weather[0].icon +".png'>"
       document.getElementById('weatherdescription').innerHTML = data.weather[0].main;
       document.getElementById('humidity').innerHTML ="Humidity"+" "+ data.main.humidity+ "%";
   }
