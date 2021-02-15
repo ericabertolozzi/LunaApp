4c02eb5cea1f250d0b118fdf10f107df56d75633
simrannambiar <71291905+simrannambiar@users.noreply.github.com>
2021-02-15 17:14:41 -0500

    Update weather.js

--- Labs/Lab2/weather.js
+++ Labs/Lab2/weather.js
@@ -14,10 +14,16 @@ $.getJSON(
   function(data) {
       document.getElementById('city').innerHTML ="<b>"+ data.name +" "+ "Weather"+"</b>"
       document.getElementById('temp').innerHTML = Math.round(data.main.temp)+ "&deg;F"
-      document.getElementById('feelslike').innerHTML ="Feels Like"+" "+ Math.round(data.main.feels_like)+ "&deg;F"
+      document.getElementById('feelslike').innerHTML ="Feels Like: "+" "+ Math.round(data.main.feels_like)+ "&deg;F"
       document.getElementById('icon').innerHTML ="<img src='http://openweathermap.org/img/wn/" + data.weather[0].icon +".png'>"
       document.getElementById('weatherdescription').innerHTML = data.weather[0].main;
-      document.getElementById('humidity').innerHTML ="Humidity"+" "+ data.main.humidity+ "%";
+      document.getElementById('humidity').innerHTML ="Humidity: "+" "+ data.main.humidity+ "%";
+      document.getElementById('pressure').innerHTML="Pressure: "+" "+ data.main.pressure + "hpa";
+      document.getElementById('tempMin').innerHTML="Temp Min: "+" "+ data.main.temp_min + "&deg;F";
+      document.getElementById('tempMax').innerHTML="Temp Max: "+" "+ data.main.temp_max + "&deg;F";
+      document.getElementById('pressure').innerHTML="Pressure: "+" "+ data.main.pressure + "hpa";
+      
+
   }
 );
 }
