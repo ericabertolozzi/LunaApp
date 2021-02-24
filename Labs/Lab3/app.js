152b06917358f45386f3dbee4093d822668598ea
barnesv17 <virginiabarnes0825@gmail.com>
2021-02-24 14:24:14 -0500

    trying to get access code

--- Labs/Lab3/app.js
+++ Labs/Lab3/app.js
@@ -1,47 +1,53 @@
-var request = require('request'); // "Request" library
+// var request = require('request'); // "Request" library
+//
+// var client_id = '0a53587a9840439ba6b8fbb87b02e74f'; // Your client id
+// var client_secret = 'client-secret'; // Your secret
+//
+// // your application requests authorization
+// var authOptions = {
+//   url: 'https://accounts.spotify.com/api/token',
+//   headers: {
+//     'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+//   },
+//   form: {
+//     grant_type: 'client_credentials'
+//   },
+//   json: true
+// };
+//
+// request.post(authOptions, function(error, response, body) {
+//   if (!error && response.statusCode === 200) {
+//
+//     // use the access token to access the Spotify Web API
+//     var token = body.access_token;
+//     var options = {
+//       url: 'https://api.spotify.com/v1/users/jmperezperez',
+//       headers: {
+//         'Authorization': 'Bearer ' + token
+//       },
+//       json: true
+//     };
+//     request.get(options, function(error, response, body) {
+//       console.log(body);
+//     });
+//   }
+// });
+//
+// // I'm thinkin we can just replace the artist ID each time and call:
+// // curl -X "GET"
+// // "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/top-tracks?market=ES"
+// // -H "Accept: application/json" -H "Content-Type: application/json"
+// // -H "Authorization: Bearer BQBO56_JagoAky4dYGVcexo0P-4frde47VYwFW0FW3zy4gtAqCDbqzW6lLu0leC8GYUt3E6yRPnMnlcwdO75s53O6Lvwj0iDk3m12joAaRSb9uWFt8GAqvh6gD7B6DX8ZM89R_3op10llQ"
+//
+// // line 2 = the API request, the random numbers is the artist ID
+// // line 3 = json stuffs
+// // line 4 = my OAuth Token
+//
+// // Maybe we can execute that shell command with node.js
+// // or we can find a way to normally call the request
 
-var client_id = '0a53587a9840439ba6b8fbb87b02e74f'; // Your client id
-var client_secret = 'client-secret'; // Your secret
+// URL encode: https%3A%2F%2Floalhost%3A3000
+// https://accounts.spotify.com/authorize?client_id=f59627e7fcb842ff8c712ebe30c3d9d7&response_type=code&redirect_uri=https%3A%2F%2Floalhost%3A3000&state=34fFs29kd09
+// spotifyApi.setAccessToken('BQBO56_JagoAky4dYGVcexo0P-4frde47VYwFW0FW3zy4gtAqCDbqzW6lLu0leC8GYUt3E6yRPnMnlcwdO75s53O6Lvwj0iDk3m12joAaRSb9uWFt8GAqvh6gD7B6DX8ZM89R_3op10llQ');
 
-// your application requests authorization
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
-
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/users/jmperezperez',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
-    });
-  }
-});
-
-// 1 I'm thinkin we can just replace the artist ID each time and call:
-// 2 curl -X "GET" 
-// 3 "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/top-tracks?market=ES" 
-// 4 -H "Accept: application/json" -H "Content-Type: application/json" 
-// 5 -H "Authorization: Bearer BQBO56_JagoAky4dYGVcexo0P-4frde47VYwFW0FW3zy4gtAqCDbqzW6lLu0leC8GYUt3E6yRPnMnlcwdO75s53O6Lvwj0iDk3m12joAaRSb9uWFt8GAqvh6gD7B6DX8ZM89R_3op10llQ"
-
-// 3 = the API request, the random numbers is the artist ID
-// 4 = json stuffs
-// 5 = my OAuth Token
-
-// Maybe we can execute that shell command with node.js
-// or we can find a way to normally call the request
+// var code = 'AQC1niWFZNVV3Gmz1jPNFiR7cCjoCO5GXWRlubjS109X6iba8E6pvXpSRls_IDmV-fXWBHHI_rW7IVSvHhQ8nEGLH0a0deGJwcFTEWicZ9hApYE0qNgsrpUUMYInyrBdHsQ7B5mOkDqMNFhHMpuqRRl3iH8VOB-1GF71QK0dWZ9QAgmURWB0kacnvOjOS13PzsvZZ2FHIsy1_ozz0gA';
--- Labs/Lab3/server.js
+++ Labs/Lab3/server.js
@@ -1,19 +1,74 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: 'f59627e7fcb842ff8c712ebe30c3d9d7',
+  clientSecret: '24e4f9a1e4244fd88fca81e8515a9860',
+  redirectUri: 'https://localhost:3000'
+});
+
+var access_token;
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  },
+  function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+  }
+);
+
+console.log( "PLEASE WORK" );
+console.log( access_token );
+
+// var spotifyApi = new SpotifyWebApi({
+//   accessToken: 'BQCF0gRKGdajztZoRc5O6uNwIFKvsTAcdFoCHpsVfBFnAdzKgSaEW0sfxpBJWhB2xfpD3U0Qst9ktmzRNgc'
+// });
+//
+// // Get an artist's top tracks
+// spotifyApi.getArtistTopTracks('0oSGxfWSnnOXhD2fKuz2Gy', 'GB')
+//   .then(function(data) {
+//     console.log(data.body);
+//     }, function(err) {
+//     console.log('Something went wrong!', err);
+//   });
+
+//------------------------------------------------------------------------------
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/pitbull', function(req, res){
+    res.send("pitbull");
+
+    // curl -X "GET"
+    // "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/top-tracks?market=ES"
+    // -H "Accept: application/json" -H "Content-Type: application/json"
+    // -H "Authorization: Bearer BQBO56_JagoAky4dYGVcexo0P-4frde47VYwFW0FW3zy4gtAqCDbqzW6lLu0leC8GYUt3E6yRPnMnlcwdO75s53O6Lvwj0iDk3m12joAaRSb9uWFt8GAqvh6gD7B6DX8ZM89R_3op10llQ"
+});
+
 
 app.listen(port, () => {
-	console.log('Listening on *:3000')
-})
+	console.log('Listening on *:3000');
+});
 
-app.get('/getsinger',urlencodedParser,function(req,res){
-    response={fname:req.body.fname}
-    response={fname:req.body.lname}
-    console.log(response)
-}
-)
+// app.get('/getsinger',urlencodedParser,function(req,res){
+//     response={fname:req.body.fname}
+//     response={fname:req.body.lname}
+//     console.log(response)
+// }
+// )
+
+
+// curl -X "GET"
+// "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/top-tracks?market=ES"
+// -H "Accept: application/json" -H "Content-Type: application/json"
+// -H "Authorization: Bearer BQBO56_JagoAky4dYGVcexo0P-4frde47VYwFW0FW3zy4gtAqCDbqzW6lLu0leC8GYUt3E6yRPnMnlcwdO75s53O6Lvwj0iDk3m12joAaRSb9uWFt8GAqvh6gD7B6DX8ZM89R_3op10llQ"
