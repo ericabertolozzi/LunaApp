caca0d32ea1705771edacbffd9c2fe920723e933
Manya <treham@rpi.edu>
2021-03-05 13:05:24 -0500

    Linked NavBar Pages

--- server.js
+++ server.js
@@ -6,4 +6,17 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/tracker.html', function(req, res){
+    res.sendFile(__dirname + '/tracker.html');
+});
+
+app.get('/explore.html', function(req, res){
+    res.sendFile(__dirname + '/explore.html');
+});
+
+app.get('/profile.html', function(req, res){
+    res.sendFile(__dirname + '/profile.html');
+});
+app.use(express.static('.'));
+
 app.listen(port, () => console.log(`Application listening on port ${port}!`))
