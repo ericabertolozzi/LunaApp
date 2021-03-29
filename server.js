91aba67decc201352ee102cab3bafbec29d04681
mcalal <47791233+mcalal@users.noreply.github.com>
2021-03-28 20:13:32 -0400

    Made everyone HTML pages and added buttons to redirect to them

--- server.js
+++ server.js
@@ -1,123 +1,52 @@
+const path = require('path');
 const express = require('express');
-const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 4200;
+app.use(express.static(path.join(__dirname, './Lab4')));
+var bodyParser = require('body-parser') //To help read form data.
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://barnev:.mUNYTL8Ga.6q2@@cluster0.pacdp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+//------------------------------------------------------------------------------
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/spotify.component.html');
 });
 
-app.get('/tracker.html', function(req, res){
-    res.sendFile(__dirname + '/tracker.html');
+app.get('/erica.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/erica.html');
 });
 
-app.get('/explore.html', function(req, res){
-    res.sendFile(__dirname + '/explore.html');
+app.get('/helena.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/helena.html');
 });
 
-app.get('/profile.html', function(req, res){
-    res.sendFile(__dirname + '/profile.html');
+app.get('/lauren.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/lauren.html');
 });
 
-app.get('/learn.html', function(req, res){
-  html1 = `
-  <html>
-  <head>
-    <title>Learn</title>
-    <link rel="icon" href="moon.png">
-    <!-- Load icon library -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./master.css"/>
-    <link rel="stylesheet" type="text/css" href="./learn.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <!-- Start of Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light" id="full-navbar">
-      <a class="navbar-brand" href="./index.html"><img src='logo.png' height='70px'/></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="./index.html">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./calendar.html">Calendar</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./tracker.html">Tracker</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./shop.html">Shop</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Learn</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./settings.html">Settings</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="./profile.html">Profile</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <!-- End of Navbar  -->
-    <div class="content" align="center">
-      <h1 class="heading">LEARN FROM OUR COLLECTION OF VERIFIED ARTICLES</h1>
-
-      <form class="example" align="center">
-        <input type="text" placeholder="Search.." name="search">
-        <button type="submit"><i class="fa fa-search"></i></button>
-      </form>
-
-      <div class="articles-container">
-  `;
-
-  html2 = `
-  </div></div>
-  <!-- Beginning of Footer  -->
-  <div class="footer">
-      <p class="footer-text">Luna Copyright &copy; 2021.</p>
-  </div>
-</body>
-</html>
-  `;
-
-  let rawdata = fs.readFileSync('learn.json');
-  let articles = JSON.parse(rawdata)["articles"];
-  var htmladd = '';
-  for( i=0; i < articles.length; i++ ) {
-    var name = articles[i]["name"];
-    var preview = articles[i]["preview"];
-    var link = articles[i]["link"];
-    htmladd += `
-      <div class="article">
-        <h1 class="text-left"><a class="name" href="`+link+`">`+name+`</a></h1>
-        <p>`+preview+`</p>
-        <div>
-          <div class="more label text-right"><a href="`+link+`">Read more</a></div>
-        </div>
-        <div class="clear"></div>
-        <hr>
-      </div>
-    `;
-  }
-  // console.log(htmladd);
-  res.write(html1+htmladd+html2);
-
-  // res.sendFile(__dirname + '/learn.html');
+app.get('/manya.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/manya.html');
 });
-app.use(express.static('.'));
 
-app.listen(port, () => console.log(`Application listening on port ${port}!`))
+app.get('/simi.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/simi.html');
+});
+
+app.get('/virginia.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/virginia.html');
+});
+
+app.listen(port, () => {
+	console.log('Listening on *:4200');
+});
+
