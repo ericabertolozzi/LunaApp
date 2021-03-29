059beb8e172ae3f11e18e3b543ec6b9f614bcfa0
mcalal <47791233+mcalal@users.noreply.github.com>
2021-03-28 20:27:21 -0400

    Add files via upload

--- Labs/Lab5/server.js
+++ Labs/Lab5/server.js
@@ -8,119 +8,42 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-// Connect to the SpotifyWebApi-------------------------------------------------
-var SpotifyWebApi = require('spotify-web-api-node');
-var spotifyApi = new SpotifyWebApi({
-  clientId: 'f59627e7fcb842ff8c712ebe30c3d9d7',
-  clientSecret: '24e4f9a1e4244fd88fca81e8515a9860',
-  redirectUri: 'https://localhost:4200'
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://barnev:.mUNYTL8Ga.6q2@@cluster0.pacdp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
-var access_token;
-// Retrieve an access token.
-spotifyApi.clientCredentialsGrant().then (
-  function(data) {
-    console.log('The access token expires in ' + data.body['expires_in']);
-    console.log('The access token is ' + data.body['access_token']);
-
-    // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-    access_token = data.body['access_token'];
-  },
-  function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
-  }
-);
-var spotifyApi = new SpotifyWebApi({
-  accessToken: access_token
-});
 //------------------------------------------------------------------------------
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/Lab4/src/app/spotify/spotify.component.html');
 });
 
-app.get('/v1/singers/:singername/returnsongs', function(req, res){
-  singername = req.param.singername;
-  spotifyApi.searchArtists( singername ) // Enter any artist name here
-    .then(function(data) {
-      var output = '';
-      artist_id = data.body['artists']['items'][0]['id']; // Get the ID from the artist name
-
-      return spotifyApi.getArtistTopTracks(artist_id, 'GB') // Get the top three tracks
-        .then(function(data) {
-          for( let i=0; i < 3; i++ ) {
-            output += data.body["tracks"][i]['name']; // Track name
-            if (i<2)
-              output += ', ';
-            // output += data.body["tracks"][i]['preview_url']; // Track audio preview
-          }
-          var htmlBegin = " <! DOCTYPE html ><html ><head ><br><br><div id='testing'><a href='https://www.spotify.com/us/'>Listen to this Artist's Top Tracks on Spotify</a></div><style > body { background-color:#354f52ec;font-size:30px;text-align:center;border:5px solid white; border-radius:30px;font-family: 'Arial', 'Helvetica', 'sans-serif'";
-          var htmlEnd = ";} #testing{font-size:20px}; </ style > </ head ><body > </ body > </ html >";
-          res.send(singername + "'s top tracks:</br>" + output + htmlBegin + htmlEnd);
-          //res.send( output );
-        }, function(err) {
-        res.send('Something went wrong!', err);
-        });
-    }, function(err) {
-      res.send("Artist name not found");
-    });
+app.get('/erica.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/erica.html');
 });
 
-
-
-var singername='';
-app.post('/v1/singers',function(req,res){
-  var htmlBegin = " <! DOCTYPE html ><html ><head ><br><br><div id='testing'><a href='https://www.spotify.com/us/'>Listen to Artist on Spotify</a></div><style > body { background-color:#354f52ec;font-size:30px;text-align:center;border:5px solid white; border-radius:30px;font-family: 'Arial', 'Helvetica', 'sans-serif'";
-  var htmlEnd = ";} #testing{font-size:20px}; </ style > </ head ><body > </ body > </ html >";
-  singername=req.body.singername;
-  res.send("<br>You Posted:"+"<br><br><b>"+singername+"</b>"+htmlBegin+htmlEnd);
-  console.log(singername) //Users input saved to variable singernames
-})
-
-
-// Test for "Pitbull"
-app.get('/v1/singers/returnsongs', function(req, res){
-  singername = req.query.singername;
-  spotifyApi.searchArtists( singername ) // Enter any artist name here
-    .then(function(data) {
-      var output = '';
-      artist_id = data.body['artists']['items'][0]['id']; // Get the ID from the artist name
-
-      return spotifyApi.getArtistTopTracks(artist_id, 'GB') // Get the top three tracks
-        .then(function(data) {
-          for( let i=0; i < 3; i++ ) {
-            output += data.body["tracks"][i]['name']; // Track name
-            if (i<2)
-              output += ', ';
-            // output += data.body["tracks"][i]['preview_url']; // Track audio preview
-          }
-          res.send(output);
-        }, function(err) {
-        res.send('Something went wrong!', err);
-        });
-    }, function(err) {
-      res.send("Artist name not found");
-    });
-})
-
-
-// PUT request to bulk update users
-app.put('/v1/users', function(req, res) {
-  res.send('PUT Request for Bulk Update Called');
+app.get('/helena.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/helena.html');
 });
 
-// PUT request to update songs for a particular singer
-app.put('/v1/singers/:singername/returnsongs', function(req, res) {
-  res.send('PUT Request for Songs Update Called');
+app.get('/lauren.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/lauren.html');
 });
 
-// Regular Expression middleware
-app.delete(/\/v1\/users\/.*a/, function(req, res) {
-  res.send('DELETE Request for users with names that contains any letters before ending with "a" called');
+app.get('/manya.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/manya.html');
 });
 
-app.delete('/v1/users', function(req, res) {
-  res.send("DELETE Request for bulk deletion of users called");
+app.get('/simi.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/simi.html');
+});
+
+app.get('/virginia.html', function(req, res){
+    res.sendFile(__dirname + '/Lab4/src/app/spotify/virginia.html');
 });
 
 app.listen(port, () => {
