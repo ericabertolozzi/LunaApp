02c13b45b982e66d8c060dc8f4c3f943ea872e35
Erica Bertolozzi <ericabertolozzi@gmail.com>
2021-04-09 13:57:49 -0400

    lab 6 setup

---
+++ Labs/Lab6/lab6/src/app/erica/erica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-erica',
+  templateUrl: './erica.component.html',
+  styleUrls: ['./erica.component.css']
+})
+export class EricaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
