b9da0b27353195a30443c7ef336d164e83b7084c
simrannambiar <71291905+simrannambiar@users.noreply.github.com>
2021-04-15 14:14:41 -0400

    Update simi.component.ts

--- Labs/Lab6/lab6/src/app/simi/simi.component.ts
+++ Labs/Lab6/lab6/src/app/simi/simi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-simi',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimiComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  public makeCSV(): void {
+    this.httpService.sendGetRequest('/display').subscribe((data) => {
+      console.log(data);
+    })
   }
 
 }
