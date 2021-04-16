7896d5ff6341deec72bbca0ecc1a68d4375b195c
barnesv17 <virginiabarnes825@gmail.com>
2021-04-15 21:27:56 -0400

    added visualizations

--- Labs/Lab6/server.js
+++ Labs/Lab6/server.js
@@ -21,7 +21,7 @@ app.get('/ericaETL', function(req, res){
         var dbo = db.db("luna");
 
         dbo.collection("Users").find({}).toArray(function(err, result) {
-            
+
 			// Convert the JSON data in 'result' to CSV
 			const csvFields = ['_id', 'age', 'app_mode'];
 			const json2csvParser = new Json2csvParser({ csvFields });
@@ -40,6 +40,35 @@ app.get('/ericaETL', function(req, res){
     });
 });
 
+app.get('/virginia', (req, res) => {
+	res.sendFile(__dirname + '/lab6/src/app/virginia/virginia.component.html');
+});
+
+app.get('/virginiaETL', function(req, res){
+	MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("luna");
+
+        dbo.collection("Articles").find({}).toArray(function(err, result) {
+
+			// Convert the JSON data in 'result' to CSV
+			const csvFields = ['_id', 'name', 'preview', 'link', 'category'];
+			const json2csvParser = new Json2csvParser({ csvFields });
+			const csv = json2csvParser.parse(result);
+
+			// have browser download csv when pressing button --NOT DONE YET
+
+			// Export the data to a physical CSV file
+			fs.writeFile('virginia-data.csv', csv, function(err) {
+				if (err) throw err;
+				console.log('File saved!');
+			});
+
+			db.close();
+        });
+    });
+});
+
 app.get('/manya', (req, res) => {
 	res.sendFile(__dirname + '/lab6/src/app/manya/manya.component.html');
 });
@@ -49,7 +78,7 @@ app.get('/display', function(req, res){
 	const fastcsv = require("fast-csv");
 	const ws = fs.createWriteStream("cycle_tracking.csv");
 	const url = "mongodb+srv://barnev:.mUNYTL8Ga.6q2%40@cluster0.pacdp.mongodb.net/luna?retryWrites=true&w=majority";
-  
+
 	mongodb.connect(
 	  url,
 	  (err, client) => {
@@ -65,7 +94,7 @@ app.get('/display', function(req, res){
 				res.send("CSV Successfully Downloaded.")
 			  })
 			  .pipe(ws);
-  
+
 			client.close();
 		  });
 	  }
