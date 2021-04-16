d1d9ecc6e99a6ad807abeb53968ab85a4b03e4ae
mcalal <47791233+mcalal@users.noreply.github.com>
2021-04-16 13:58:46 -0400

    Update lauren.component.ts

--- Labs/Lab6/lab6/src/app/lauren/lauren.component.ts
+++ Labs/Lab6/lab6/src/app/lauren/lauren.component.ts
@@ -19,4 +19,15 @@ export class LaurenComponent implements OnInit {
     })
   }
 
+  public makeFirstDataSet(): void {
+    this.httpService.sendGetRequest('/ericaETL1').subscribe((data) => {
+      console.log(data);
+    })
+  }
+
+  public makeSecondDataSet(): void {
+    this.httpService.sendGetRequest('/ericaETL2').subscribe((data) => {
+      console.log(data);
+    })
+  }
 }
