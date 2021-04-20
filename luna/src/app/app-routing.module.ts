60495da26e9f5c37bb380aa42df851fce9d59822
Manya <treham@rpi.edu>
2021-04-20 09:29:01 -0400

    Update app-routing.module.ts

--- luna/src/app/app-routing.module.ts
+++ luna/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import 'testing.js'
+import 'testing.js';
 
 declare var showInput: any;
 
@@ -8,10 +8,6 @@ declare var showInput: any;
 
 import { LearnComponent } from './learn/learn.component';
 import { TrackingComponent } from './tracking/tracking.component';
-
-const routes: Routes = [
-  { path: 'learn', component: LearnComponent },
-  { path: 'tracking',component: TrackingComponent },
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
@@ -21,6 +17,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'tracking',component: TrackingComponent },
 
 
 ];
