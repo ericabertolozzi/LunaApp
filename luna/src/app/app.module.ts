cf7402fde09978d03a6191ae44413db1a3b275df
Manya <treham@rpi.edu>
2021-04-29 19:35:03 -0400

    Fixed errors...everything working

--- luna/src/app/app.module.ts
+++ luna/src/app/app.module.ts
@@ -1,20 +1,17 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SafePipeModule } from 'safe-pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LearnComponent } from './learn/learn.component';
 import { HomeComponent } from './home/home.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 import { AjaxComponent } from './ajax/ajax.component';
 import { TrackingComponent } from './tracking/tracking.component';
-import { ShoppingComponent } from './shopping/shopping.component';
 import { CalendarComponent } from './calendar/calendar.component';
-import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -26,20 +23,17 @@ import { SettingsComponent } from './settings/settings.component';
     TrackingComponent,
     ShoppingComponent,
     CalendarComponent,
-    SettingsComponent
   ],
-  imports: [ 
+  imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SafePipeModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
 
 
 
